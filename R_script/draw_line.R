cf60d827e771fed40d7490c72a064a327804e724
raisok <454418154@qq.com>
2018-03-13 14:11:11 +0800

    循环画折线图

--- R_script/draw_line.R
+++ R_script/draw_line.R
@@ -2,19 +2,20 @@
 #gene1 = log10(gene)
 #gene1[gene1 == "-Inf"] =0
 #gene1[gene1 >= 20] =20
+
 setwd("c:/Users/yueyao/Desktop/latex_cluster/")
 for(j in 1:10){
-input=paste("cluster",j,".fpkm.txt",sep="")
-output=paste("cluster",j,".pdf",sep="")
-gene1 = read.table(file=input,header = T,row.names = 1)
-x=c(1:27)
-y= c(gene1[1,])
-pdf(output)
-plot(x,y,type = "l",xlab = "Species", ylab="fpkm")
-len = nrow(gene1)
-for(i in 2:len){
-a=c(gene1[i,])
-lines(x,a)
-}
-dev.off()
+    input=paste("cluster",j,".fpkm.txt",sep="")
+    output=paste("cluster",j,".pdf",sep="")
+    gene1 = read.table(file=input,header = T,row.names = 1)
+    x=c(1:27)
+    y= c(gene1[1,])
+    pdf(output)
+    plot(x,y,type = "l",xlab = "Species", ylab="fpkm")
+    len = nrow(gene1)
+    for(i in 2:len){
+        a=c(gene1[i,])
+        lines(x,a)
+    }
+    dev.off()
 }
